ee9e5f6aa6dec926b561756bec9f21ea2433f6d8
Guy Van den Nieuwenhof <guyvdn@gmail.com>
2020-12-19 01:17:50 +0100

    Beta: The_speed_of_letters

---
+++ src/Beta/The_speed_of_letters/Kata.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Codewars.Beta
+{
+    /// <summary>
+    /// The Speed of Letters
+    /// https://www.codewars.com/kata/5fc7caa854783c002196f2cb/train/csharp
+    /// </summary>
+    public static class Kata
+    {
+        public static string Speedify(string input)
+        {
+            var result = new string(' ', input.Length + 25).ToCharArray();
+            const int a = 'A';
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                result[input[i] - a + i] = input[i];
+            }
+
+            return new string(result).TrimEnd(' ');
+        }
+    }
+
+    [TestFixture]
+    public class KataTests
+    {
+        [TestCase("AZ", ExpectedResult = "A                         Z")]
+        [TestCase("ABC", ExpectedResult = "A B C")]
+        [TestCase("ACE", ExpectedResult = "A  C  E")]
+        [TestCase("CBA", ExpectedResult = "  A")]
+        [TestCase("HELLOWORLD", ExpectedResult = "     E H    DLL   OLO   R  W")]
+        public string BasicTests(string input)
+        {
+            return Kata.Speedify(input);
+        }
+    }
+}
